a49c723b95e12cf9a360d230f29e66a239d732ae
Michael Harmon <mwharmon1@dmacc.edu>
2019-09-19 22:34:31 -0500

    Final Commit Added user input with loop and try except to catch any bad input that will raise a value error. Tests are passing.

--- input_loops/average_input_scores.py
+++ input_loops/average_input_scores.py
@@ -16,5 +16,25 @@ def average(score_list):
 
 
 if __name__ == '__main__':
+    last_name = input("Enter your last name: ")
+    first_name = input("Enter you first name: ")
     scores = []
-    print(average(scores))
+    score = 0
+    sentinel = -1
+    while score >= 0:
+        try:
+            score = float(input("Please enter a score or -1 when finished: "))
+        except ValueError:
+            print("Numbers only")
+        else:
+            if score == sentinel:
+                break
+            else:
+                scores.append(score)
+    avg_scores = average(scores)
+    print(last_name + ', ' + first_name + ' grade:' '% 5.2f' % avg_scores)
+
+"""My code is prompting user for name
+and scores. If a string is entered a value error is raised
+and a message is displayed.
+"""
--- test/test_loop.py
+++ test/test_loop.py
@@ -12,6 +12,20 @@ class MyTestCase(unittest.TestCase):
     def test_average(self):
         self.assertEqual(93.75, a.average([95, 99, 93, 88]))
 
+    def test_average_not_equal(self):
+        self.assertNotEqual(99, a.average([91, 88, 96, 81]))
+
+    def test_average_floats(self):
+        self.assertEqual(89.88, a.average([92.3, 91.6, 89.2, 86.4]))
+
+    def test_average_not_equal_floats(self):
+        self.assertNotEqual(91.34, a.average([99.4, 87.5, 93.1]))
+
 
 if __name__ == '__main__':
     unittest.main()
+"""My unit tests are testing for equal and not equal
+    with int and floating point values.
+    I added round 2 to my average() because my 
+    tests were not rounding up as my function was.
+"""
